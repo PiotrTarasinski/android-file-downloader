6e217e4f9a55924fb1031257044fcd24d25839c4
Piotr <piotr@piotr.tarasinski>
2019-06-11 01:54:49 +0200

    added ui

--- file-download/src/app/home/home.page.ts
+++ file-download/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  fileUrl = 'https://drive.google.com/uc?export=download&id=1ApRtzrFgLgejae8OBmcYfeiJuhpmW_8F';
+  fileSize = 'zxczxczxc';
+  fileType = 'zxczxczxc';
+  bytesDownloaded: number;
+  progress: number;
+
+  constructor(
+    private http: HttpClient,
+  ) {}
+
+
+  getFileInfo = () => {
+    return this.http.get(this.fileUrl)
+    .subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (error) => {
+
+      }
+    );
+    console.log(this.fileUrl);
+  }
+
+  downloadFile = () => {
+
+  }
 
 }
