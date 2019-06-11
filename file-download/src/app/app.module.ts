cead14ae88e9128cbfe69810552f42c91d4c7914
Piotr <piotr@piotr.tarasinski>
2019-06-11 02:32:19 +0200

    added getFileInfo function

--- file-download/src/app/app.module.ts
+++ file-download/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { HTTP } from '@ionic-native/http/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -17,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    HTTP,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- file-download/src/app/home/home.page.ts
+++ file-download/src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import { HTTP } from '@ionic-native/http/ngx';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,28 +8,33 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
 
-  fileUrl = 'https://drive.google.com/uc?export=download&id=1ApRtzrFgLgejae8OBmcYfeiJuhpmW_8F';
-  fileSize = 'zxczxczxc';
-  fileType = 'zxczxczxc';
+  fileUrl = 'http://ipv4.download.thinkbroadband.com/20MB.zip';
+  fileSize = '';
+  fileType = '';
   bytesDownloaded: number;
   progress: number;
 
   constructor(
-    private http: HttpClient,
+    private http: HTTP,
+    public toastController: ToastController
   ) {}
 
 
   getFileInfo = () => {
-    return this.http.get(this.fileUrl)
-    .subscribe(
-      (res) => {
-        console.log(res);
-      },
-      (error) => {
+    this.http.head(this.fileUrl, {}, {})
+      .then(async (data) => {
+        this.fileSize = (data.headers['content-length'] * Math.pow(10, -6)).toFixed(2).toString() + ' Mb';
+        this.fileType = data.headers['content-type'];
+        const toast = await this.toastController.create({
+          message: 'Downloaded file info',
+          duration: 2000
+        });
+        toast.present();
+      })
+      .catch(error => {
+        console.log(error);
 
-      }
-    );
-    console.log(this.fileUrl);
+      });
   }
 
   downloadFile = () => {
